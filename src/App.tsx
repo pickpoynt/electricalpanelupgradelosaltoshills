a8b0a89e0053903ed2ef4a80d7556dd46daa2392
PickPoynt Admin <info@pickpoynt.com>
2026-01-24 12:26:08 +0530

    feat: add persistent floating call button

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import FloatingCallButton from "./components/ui/FloatingCallButton";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import FloodedGarageCleanup from "./pages/FloodedGarageCleanup";
@@ -18,6 +19,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
+      <FloatingCallButton />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
